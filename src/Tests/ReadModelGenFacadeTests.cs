b44809fa195cc29743a0d7264a2e54d0464d32ca
Mihai Mogosanu <mike@sapiensworks.com>
2017-09-06 12:25:06 -0400

    read model generation tests done

--- src/Tests/ReadModelGenFacadeTests.cs
+++ src/Tests/ReadModelGenFacadeTests.cs
@@ -2,6 +2,10 @@
 using FluentAssertions;
 using Xunit;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using DominoEventStore;
+using NSubstitute;
 using Xunit.Abstractions;
 
 
@@ -10,18 +14,37 @@ namespace Tests
     public class ReadModelGenFacadeTests
     {
         private readonly ITestOutputHelper _h;
+        private ISpecificDbStorage _storage;
+        private StoreFacade _sut;
+        private ReadModelGenerationConfig _config;
 
         public ReadModelGenFacadeTests(ITestOutputHelper h)
         {
             _h = h;
+            _storage = Substitute.For<ISpecificDbStorage>();
+            _sut = new StoreFacade(_storage, Setup.EventStoreSettings);
+            _config=new ReadModelGenerationConfig("test");            
+        }
+
+        void SetupCommits()
+        {
+            _storage.StartOrContinue("test").Returns(new ProcessedCommitsCount(0));
+
+            _storage.GetNextBatch(Arg.Any<ReadModelGenerationConfig>(), 2)
+                .Returns(new CommittedEvents(new Commit[0]));
+            _storage.GetNextBatch(Arg.Any<ReadModelGenerationConfig>(), 0)
+                .Returns(Setup.CommittedEvents<Event1, Event2>(2));
         }
 
         [Fact]
-        public void r()
+        public void read_model_function_is_invoked_for_each_event()
         {
-            Action<dynamic> e = ev => _h.WriteLine(ev.GetType().Name);
-            e(new Event1());
-            e(new Event2());
+            var r=new List<Type>();
+            SetupCommits();
+            _sut.GenerateReadModel("test",e=>{ r.Add(e.GetType());});
+            
+            r.Count(d => d == typeof(Event1)).Should().Be(2);
+            r.Count(d => d == typeof(Event2)).Should().Be(2);
         }
 
 
--- src/Tests/Setup.cs
+++ src/Tests/Setup.cs
@@ -14,11 +14,16 @@ namespace Tests
 
         public static readonly Guid EntityId = Guid.NewGuid();
 
-        public static IEnumerable<Commit> Commits(int count)
+        public static IEnumerable<Commit> Commits(int count) => Commits<SomeEvent, SomeEvent>(count);
+
+        public static IEnumerable<Commit> Commits<T,V>(int count) where T : class, new() where V : class, new()
         {
             return Enumerable.Range(1, count)
-                .Select(i => new Commit("_", Setup.EntityId, Utils.PackEvents(new SomeEvent(), new SomeEvent()), Guid.NewGuid(), DateTimeOffset.Now, i));
+                .Select(i => new Commit("_", Setup.EntityId, Utils.PackEvents(new T(), new V()), Guid.NewGuid(), DateTimeOffset.Now, i));
         }
+
+        public static CommittedEvents CommittedEvents<T, V>(int count) where T : class, new() where V : class, new()
+            => new CommittedEvents(Setup.Commits<T, V>(count).ToArray());
 
     }
 }
